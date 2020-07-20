b4d350530cc11216b7b68fb79e0e242249c7d4ad
Giovanni Insfran <gioinsfran98@gmail.com>
2020-07-20 12:14:21 -0400

    Modificando Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './scss/style.scss';
 
+
 import { TheFooter, TheHeader } from './containers/index'
 
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div>
       <TheHeader />
-      
+    
       <TheFooter />
     </div>
   );
