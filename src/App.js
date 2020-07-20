bfbf67d9cd1a31df048dd2f5d081eef7934e375c
Giovanni Insfran <gioinsfran98@gmail.com>
2020-07-20 12:23:55 -0400

    Modificando Header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import './scss/style.scss';
 
@@ -12,6 +13,34 @@ function App() {
       <TheHeader />
     
       <TheFooter />
+=======
+import React from "react";
+import { CFooter } from "@coreui/react";
+import "./scss/style.scss";
+// COMENTARIO
+function App() {
+  return (
+    <div>
+      <h1>Test React App</h1>
+      <CFooter fixed={false}>
+        <div>
+          <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
+            CoreUI
+          </a>
+          <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        </div>
+        <div className="mfs-auto">
+          <span className="mr-1">Powered by</span>
+          <a
+            href="https://coreui.io/react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CoreUI for React
+          </a>
+        </div>
+      </CFooter>
+>>>>>>> d87dda80a73cb7e4d98e1e74a165f770774791d4
     </div>
   );
 }
